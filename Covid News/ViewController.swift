5ed69b092140dd7ce93b309b1d44e4c6e4740a4c
Alex Paul <lexypaul13@yahoo.com>
2020-08-20 22:48:58 -0400

    Initial Commit

---
+++ Covid News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid News
+//
+//  Created by Alex Paul on 8/20/20.
+//  Copyright © 2020 Alexander Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
