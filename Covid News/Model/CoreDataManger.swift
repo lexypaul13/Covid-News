a113e943fe1b1e3e9f255e9d8865f2a6a14b1f64
David Razmadze <razd45@gmail.com>
2020-12-03 10:42:06 -0500

    Add back in createData().

--- Covid News/Model/CoreDataManger.swift
+++ Covid News/Model/CoreDataManger.swift
@@ -11,14 +11,18 @@ import CoreData
 
 class CoreDataManger: NSObject {
   
+  // MARK: - Properties
+  
   static let sharedInstance = CoreDataManger()
-  private override init() {}
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+  let persistentContainer = (UIApplication.shared.delegate as! AppDelegate).persistentContainer
   
   var newsCoreData: [News] = []
   var article = ArticlesData()
   
-  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-  let persistentContainer = (UIApplication.shared.delegate as! AppDelegate).persistentContainer
+  // MARK: - Lifecycle
+  
+  private override init() {}
   
   // MARK: - Core Data Saving support
   
@@ -54,14 +58,45 @@ class CoreDataManger: NSObject {
     print("Core Data DB Path: \(path ?? "Not found")")
   }
   
+  
+  
+  // MARK: - Core Data Saving support
+  
+  func createData() {
+    
+    //As we know that container is set up in the AppDelegates so we need to refer that container.
+    DispatchQueue.main.async(execute: { [self] in
+      guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+      
+      //We need to create a context from this container
+      let managedContext = appDelegate.persistentContainer.viewContext
+      
+      //Now let’s create an entity and new user records.
+      let newsEntity = NSEntityDescription.entity(forEntityName: "News", in: managedContext)!
+      let news = NSManagedObject(entity: newsEntity, insertInto: managedContext)
+      
+      for article in self.newsCoreData{
+        news.setValue("\(article.author ?? "")", forKeyPath: "author")
+        news.setValue("\(article.myDescription ?? "")", forKeyPath: "myDescription")
+        news.setValue("\(article.publishedAt ?? "")", forKeyPath: "publishedAt")
+        news.setValue("\(article.title ?? "")", forKeyPath: "title")
+        news.setValue("\(article.urlImage ?? "")", forKeyPath: "urlImage")
+        news.setValue("\(article.urlWebsite ?? "")", forKeyPath: "urlWebsite")
+        
+      }
+      
+      //Now we have set all the values. The next step is to save them inside the Core Data
+      
+      do {
+        try managedContext.save()
+        newsCoreData.append(news as! News)
+        
+      } catch let error as NSError {
+        print("Could not save. \(error), \(error.userInfo)")
+      }
+      print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+      
+    }
+    )}
+  
 }
-
-//extension CoreDataStack {
-//
-//    func applicationDocumentsDirectory() {
-//        // The directory the application uses to store the Core Data store file. This code uses a directory named "yo.BlogReaderApp" in the application's documents directory.
-//        if let url = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).last {
-//            print(url.absoluteString)
-//        }
-//    }
-//}
