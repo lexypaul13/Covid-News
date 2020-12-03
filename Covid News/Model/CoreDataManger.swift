42d20bccbb559506a065bdc10abf676665736cf7
David Razmadze <razd45@gmail.com>
2020-12-03 09:58:25 -0500

    Print CoreData path. You can view it by copying the path and opening it in Finder with CMD+SHIFT+G.

--- Covid News/Model/CoreDataManger.swift
+++ Covid News/Model/CoreDataManger.swift
@@ -10,51 +10,64 @@ import UIKit
 import CoreData
 
 class CoreDataManger: NSObject {
+  
+  static let sharedInstance = CoreDataManger()
+  private override init() {}
+  
+  var newsCoreData: [News] = []
+  var article = ArticlesData()
+  
+  // MARK: - Core Data Saving support
+  
+  func createData(){
     
-    static let sharedInstance = CoreDataManger()
-    private override init() {}
-    
-    var newsCoreData: [News] = []
-    var article = ArticlesData()
- 
-    // MARK: - Core Data Saving support
-    
-    func createData(){
+    //As we know that container is set up in the AppDelegates so we need to refer that container.
+    DispatchQueue.main.async(execute: { [self] in
+      guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+      
+      //We need to create a context from this container
+      let managedContext = appDelegate.persistentContainer.viewContext
+      
+      //Now let’s create an entity and new user records.
+      let newsEntity = NSEntityDescription.entity(forEntityName: "News", in: managedContext)!
+      let news = NSManagedObject(entity: newsEntity, insertInto: managedContext)
+      
+      for article in self.newsCoreData{
+        news.setValue("\(article.author ?? "")", forKeyPath: "author")
+        news.setValue("\(article.myDescription ?? "")", forKeyPath: "myDescription")
+        news.setValue("\(article.publishedAt ?? "")", forKeyPath: "publishedAt")
+        news.setValue("\(article.title ?? "")", forKeyPath: "title")
+        news.setValue("\(article.urlImage ?? "")", forKeyPath: "urlImage")
+        news.setValue("\(article.urlWebsite ?? "")", forKeyPath: "urlWebsite")
         
-        //As we know that container is set up in the AppDelegates so we need to refer that container.
-        DispatchQueue.main.async(execute: { [self] in
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+      }
+      
+      //Now we have set all the values. The next step is to save them inside the Core Data
+      
+      do {
+        try managedContext.save()
+        newsCoreData.append(news as! News)
         
-        //We need to create a context from this container
-        let managedContext = appDelegate.persistentContainer.viewContext
-        
-        //Now let’s create an entity and new user records.
-        let newsEntity = NSEntityDescription.entity(forEntityName: "News", in: managedContext)!
-        let news = NSManagedObject(entity: newsEntity, insertInto: managedContext)
-
-        for article in self.newsCoreData{
-         news.setValue("\(article.author ?? "")", forKeyPath: "author")
-         news.setValue("\(article.myDescription ?? "")", forKeyPath: "myDescription")
-         news.setValue("\(article.publishedAt ?? "")", forKeyPath: "publishedAt")
-         news.setValue("\(article.title ?? "")", forKeyPath: "title")
-         news.setValue("\(article.urlImage ?? "")", forKeyPath: "urlImage")
-         news.setValue("\(article.urlWebsite ?? "")", forKeyPath: "urlWebsite")
- 
-        }
-        
-       //Now we have set all the values. The next step is to save them inside the Core Data
-        
-        do {
-            try managedContext.save()
-            newsCoreData.append(news as! News)
-            
-        } catch let error as NSError {
-            print("Could not save. \(error), \(error.userInfo)")
-        }
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-
+      } catch let error as NSError {
+        print("Could not save. \(error), \(error.userInfo)")
+      }
+      print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+      
     }
-        )}
+    )}
+  
+  func printCoreDataDBPath() {
+    let path = FileManager
+      .default
+      .urls(for: .applicationSupportDirectory, in: .userDomainMask)
+      .last?
+      .absoluteString
+      .replacingOccurrences(of: "file://", with: "")
+      .removingPercentEncoding
+    print("Core Data DB Path: \(path ?? "Not found")")
+  }
+  
+  
 }
 
 
