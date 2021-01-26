91255d6442f8e08ec5e4f4406c80598e990e62ca
Alex Paul <lexypaul13@yahoo.com>
2021-01-26 01:01:33 -0500

    x
    x

---
+++ Pods/Target Support Files/Pods-Covid News-Covid NewsUITests/Pods-Covid News-Covid NewsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Covid_News_Covid_NewsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Covid_News_Covid_NewsUITests
+@end
---
+++ Pods/Target Support Files/Pods-Covid NewsTests/Pods-Covid NewsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Covid_NewsTests : NSObject
+@end
+@implementation PodsDummy_Pods_Covid_NewsTests
+@end
